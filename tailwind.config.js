fd65b64339f8ad5faf12476666acbf2e5a7495f3
Haruka Ichinose <Haruka.Ichinose@umajin.com>
2023-06-10 14:13:28 +1200

    Tailwind CSS theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-      }
+        primary: '#AED6D6',
+        secondary: '#D2FFAD',
+        accent: '#FDC4C0'
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'], // Custom font stack
+      },
     },
   },
   plugins: [],
